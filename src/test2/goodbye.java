f183d0523ab67f173124f07718cb9e6b3d67bace
weerapon <weerapon1991@hotmail.com>
2013-08-21 11:26:43 +0700

    Edit And Add
    second change

--- src/test2/goodbye.java
+++ src/test2/goodbye.java
@@ -1,11 +1,10 @@
 package test2;
 
-public class hello {
+public class goodbye {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello word");
-		System.out.println("End.");
+		System.out.println("goodbye");
 	}
 
 }
